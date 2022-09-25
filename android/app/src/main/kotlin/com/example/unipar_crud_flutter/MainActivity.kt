89067b595e64cd93dbe9c4918cb860e43b6e643b
Margot Paon Garcia <margot.paon@gmail.com>
2022-09-25 11:52:54 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/unipar_crud_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.unipar_crud_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key:key);
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(appBar: AppBar(title: const Text('Crud Unipar'),),);
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    title:'Flutter Demo',
+    theme: ThemeData(primarySwatch: Colors.blue,),
+    home: const HomePage(),
+  ),);
+}
+
