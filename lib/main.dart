e963f6ead2e99ccf34ca3179a619c4d41ce02f08
Margot Paon Garcia <margot.paon@gmail.com>
2022-09-25 12:22:44 -0300

    Clean main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
-  const HomePage({Key? key}) : super(key:key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context){
-    return Scaffold(appBar: AppBar(title: const Text('Crud Unipar'),),);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Crud Unipar'),
+      ),
+    );
   }
 }
 
 void main() {
-  runApp(MaterialApp(
-    title:'Flutter Demo',
-    theme: ThemeData(primarySwatch: Colors.blue,),
-    home: const HomePage(),
-  ),);
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    ),
+  );
 }
-
