60b2317f9d4c74aae9606a66d26d6e490febd045
Margot Paon Garcia <margot.paon@gmail.com>
2022-09-25 23:07:01 -0300

    add methods in model

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -7,25 +7,32 @@ class ProductModel implements Comparable {
   String? quantidade = "";
 
   ProductModel({
-    this.id,
-    this.name,
-    this.tamanho,
-    this.cor,
-    this.preco,
-    this.quantidade,
+    required this.id,
+    required this.name,
+    required this.tamanho,
+    required this.cor,
+    required this.preco,
+    required this.quantidade,
   });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'tamanho': tamanho,
-      'cor': cor,
-      'preco': preco,
-      'quantidade': quantidade,
-    };
-  }
+  ProductModel.fromRow(Map<String, Object?> row)
+      : id = row['ID'] as int,
+        name = row['NAME'] as String,
+        tamanho = row['TAMANHO'] as String,
+        cor = row['COR'] as String,
+        preco = row['PRECO'] as String,
+        quantidade = row['QUANTIDADE'] as String;
 
   @override
   int compareTo(covariant ProductModel other) => other.id!.compareTo(id!);
+
+  @override
+  bool operator ==(covariant ProductModel other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+
+  @override
+  String toString() =>
+      'Product, id=$id, name:$name, tamanho:$tamanho, cor:$cor, preco:$preco, quantidade:$quantidade';
 }
